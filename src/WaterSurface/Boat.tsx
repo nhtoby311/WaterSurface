ec87e8aeb14de6b12353d0d12927dd87054ff325
nvminhhieu <nvminhhieu1999@gmail.com>
2024-03-27 12:32:18 +0100

    fix build

--- src/WaterSurface/Boat.tsx
+++ src/WaterSurface/Boat.tsx
@@ -1,8 +1,21 @@
+import * as THREE from 'three';
 import React, { useRef } from 'react';
 import { useGLTF } from '@react-three/drei';
+import { GLTF } from 'three-stdlib';
 
-export function Boat(props) {
-	const { nodes, materials } = useGLTF('/boat.glb');
+type GLTFResult = GLTF & {
+	nodes: {
+		Plane004_Boat_0_1: THREE.Mesh;
+		Plane004_Boat_0_2: THREE.Mesh;
+	};
+	materials: {
+		Surface: THREE.MeshStandardMaterial;
+		cloth: THREE.MeshStandardMaterial;
+	};
+};
+
+export function Boat(props: JSX.IntrinsicElements['group']) {
+	const { nodes, materials } = useGLTF('/boat.glb') as GLTFResult;
 	return (
 		<group {...props} dispose={null}>
 			<mesh
